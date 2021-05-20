606504ae852b12c81e93c074247e48e4d541456b
Joshua Reed <joshuareed@workfront.com>
2021-05-19 23:08:13 -0600

    Don't use var

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,3 +3,10 @@
 var nameVar = "Josh";
 nameVar = "Flint";
 console.log("nameVar", nameVar);
+
+var nameLet = "Jen";
+nameLet = "Ken";
+console.log(nameLet);
+
+var nameConst = "Frank";
+console.log(nameConst);
--- indecision-app/src/playground/es6-let-const.js
+++ indecision-app/src/playground/es6-let-const.js
@@ -1,3 +1,10 @@
 var nameVar = "Josh";
 nameVar = "Flint";
 console.log("nameVar", nameVar);
+
+let nameLet = "Jen";
+nameLet = "Ken";
+console.log(nameLet);
+
+const nameConst = "Frank";
+console.log(nameConst);
