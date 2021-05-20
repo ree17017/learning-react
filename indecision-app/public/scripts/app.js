03a507482940f2de52bd50e67213de293d67db91
Joshua Reed <joshuareed@workfront.com>
2021-05-19 23:31:18 -0600

    Challenge write Fat Arrow function

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,12 +1,17 @@
 "use strict";
 
-var square = function square(x) {
-    return x * x;
+// const square = function (x) {
+//     return x * x;
+// };
+
+// const suareArrow = (x) => x * x;
+
+// console.log(square(8));
+
+// console.log(suareArrow(8));
+
+var getFirstName = function getFirstName(firstName) {
+  return firstName.split(" ")[0];
 };
 
-var suareArrow = function suareArrow(x) {
-    return x * x;
-};
-
-console.log(square(8));
-console.log(suareArrow(8));
+console.log(getFirstName("Joshua Reed"));
--- indecision-app/src/playground/es6-arrow-function.js
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -1,8 +1,13 @@
-const square = function (x) {
-    return x * x;
-};
+// const square = function (x) {
+//     return x * x;
+// };
 
-const suareArrow = (x) => x * x;
+// const suareArrow = (x) => x * x;
 
-console.log(square(8));
-console.log(suareArrow(8));
+// console.log(square(8));
+
+// console.log(suareArrow(8));
+
+const getFirstName = (firstName) => firstName.split(" ")[0];
+
+console.log(getFirstName("Joshua Reed"));
