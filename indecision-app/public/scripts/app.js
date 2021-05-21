87dd28efdf3530e780e9c3cfd002b776ac62f6c9
Joshua Reed <joshuareed@workfront.com>
2021-05-21 15:18:39 -0600

    Multiplier

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -11,7 +11,44 @@
 // console.log(suareArrow(8));
 
 var getFirstName = function getFirstName(firstName) {
-  return firstName.split(" ")[0];
+    return firstName.split(" ")[0];
 };
 
 console.log(getFirstName("Joshua Reed"));
+
+var add = function add(a, b) {
+    return a + b;
+};
+
+console.log(add(1, 2));
+
+var user = {
+    name: "Josh",
+    cities: ["Turlock", "Modesto", "Santaquin"],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        return this.cities.map(function (city) {
+            return _this.name + " has livined in " + city;
+        });
+    }
+};
+
+console.log(user.printPlacesLived());
+
+var multiplier = {
+    numbers: [1, 2, 3],
+    multiplyBy: 2,
+    multiply: function multiply() {
+        var _this2 = this;
+
+        var numbers = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.numbers;
+        var multiplyBy = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : this.multiplyBy;
+
+        return this.numbers.map(function (num) {
+            return num * _this2.multiplyBy;
+        });
+    }
+};
+
+console.log(multiplier.multiply());
--- indecision-app/src/playground/es6-arrow-function.js
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -11,3 +11,31 @@
 const getFirstName = (firstName) => firstName.split(" ")[0];
 
 console.log(getFirstName("Joshua Reed"));
+
+const add = function (a, b) {
+    return a + b;
+};
+
+console.log(add(1, 2));
+
+const user = {
+    name: "Josh",
+    cities: ["Turlock", "Modesto", "Santaquin"],
+    printPlacesLived() {
+        return this.cities.map(
+            (city) => this.name + " has livined in " + city
+        );
+    },
+};
+
+console.log(user.printPlacesLived());
+
+const multiplier = {
+    numbers: [1, 2, 3],
+    multiplyBy: 2,
+    multiply(numbers = this.numbers, multiplyBy = this.multiplyBy) {
+        return this.numbers.map((num) => num * this.multiplyBy);
+    },
+};
+
+console.log(multiplier.multiply());
